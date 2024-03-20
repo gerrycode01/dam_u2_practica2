60c8293a6170cedad9a39c474bcc658c673a16ae
Gespinosa <gespinosa01@icloud.com>
2024-03-19 22:57:36 -0700

    First commit

---
+++ android/app/src/main/kotlin/mx/edu/ittepic/dam_u2_tarea2/MainActivity.kt
@@ -0,0 +1,5 @@
+package mx.edu.ittepic.dam_u2_tarea2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Login(),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Login',
+          style: TextStyle(
+              color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: ,
+    );
+  }
+}
